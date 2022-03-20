ffa3728360fa3ad3115bc7192474f48db2f56847
ryanpaynt <ryanpaynt@outlook.com>
2022-03-20 16:03:00 -0400

    working on homepage css/html

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -10,6 +10,9 @@ module.exports = (app) => {
   // Below code handles when users "visit" a page.
   // In each of the below cases the user is shown an HTML page of content
 
+  app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/graphPage.html'));
+  });
   app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/home.html'));
   });
