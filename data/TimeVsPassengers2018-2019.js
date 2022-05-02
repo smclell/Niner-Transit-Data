bec35b75c7ba64a28142990184abe67a50b0ee29
bbrickne <bbrickne@uncc.edu>
2022-05-02 12:54:31 -0400

    Added TimeVsPassenfers2018-2019.js data set to be used

---
+++ data/TimeVsPassengers2018-2019.js
@@ -0,0 +1,101 @@
+const TimeVsPassengers =
+  [
+    {
+      "Time": "12:00 AM",
+      "Riders": 13301
+    },
+    {
+      "Time": "1:00 AM",
+      "Riders": 8699
+    },
+    {
+      "Time": "2:00 AM",
+      "Riders": 248
+    },
+    {
+      "Time": "3:00 AM",
+      "Riders": 0
+    },
+    {
+      "Time": "4:00 AM",
+      "Riders": 0
+    },
+    {
+      "Time": "5:00 AM",
+      "Riders": 0
+    },
+    {
+      "Time": "6:00 AM",
+      "Riders": 13546
+    },
+    {
+      "Time": "7:00 AM",
+      "Riders": 47976
+    },
+    {
+      "Time": "8:00 AM",
+      "Riders": 89153
+    },
+    {
+      "Time": "9:00 AM",
+      "Riders": 100327
+    },
+    {
+      "Time": "10:00 AM",
+      "Riders": 99452
+    },
+    {
+      "Time": "11:00 AM",
+      "Riders": 120903
+    },
+    {
+      "Time": "12:00 PM",
+      "Riders": 130526
+    },
+    {
+      "Time": "1:00 PM",
+      "Riders": 117673
+    },
+    {
+      "Time": "2:00 PM",
+      "Riders": 127797
+    },
+    {
+      "Time": "3:00 PM",
+      "Riders": 110768
+    },
+    {
+      "Time": "4:00 PM",
+      "Riders": 84689
+    },
+    {
+      "Time": "5:00 PM",
+      "Riders": 104281
+    },
+    {
+      "Time": "6:00 PM",
+      "Riders": 82810
+    },
+    {
+      "Time": "7:00 PM",
+      "Riders": 68861
+    },
+    {
+      "Time": "8:00 PM",
+      "Riders": 59953
+    },
+    {
+      "Time": "9:00 PM",
+      "Riders": 48122
+    },
+    {
+      "Time": "10:00 PM",
+      "Riders": 31260
+    },
+    {
+      "Time": "11:00 PM",
+      "Riders": 19457
+    }
+  ]
+
+module.exports = TimeVsPassengers;
